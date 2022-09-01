a3bf2464ce2505d4f0ddcd2082080e221b9f920c
fras60 <112525432+fras60@users.noreply.github.com>
2022-09-01 04:17:04 +0300

    Update cake.sh

--- cake.sh
+++ cake.sh
@@ -21,8 +21,8 @@ DOWN_METHOD="veth"  # Write: "veth" | "normal"
 
 ### CAKE settings ###
 
-BANDWIDTH_DOWN="42"  # Change this to about 80-95% of your download speed (in megabits).
-BANDWIDTH_UP="20"     # Change this to about 80-95% of your upload speed (in megabits).
+BANDWIDTH_DOWN="48"  # Change this to about 80-95% of your download speed (in megabits).
+BANDWIDTH_UP="21"     # Change this to about 80-95% of your upload speed (in megabits).
                       # Do a Speed Test: https://www.speedtest.net/
                       # Not recommendable: Write "0" in "BANDWIDTH_DOWN" or "BANDWIDTH_UP" to use 'CAKE' with no limit on the bandwidth ('unlimited' parameter).
                       # Not recommendable: Don't write anything in "BANDWIDTH_DOWN" or "BANDWIDTH_UP" to disable 'shaping' on ingress or egress.
