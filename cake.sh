081667a94adabee3652393c8d1fecf9ac520345c
fras60 <112525432+fras60@users.noreply.github.com>
2022-11-20 15:21:28 +0300

    Update cake.sh

--- cake.sh
+++ cake.sh
@@ -5,7 +5,7 @@
 ### Interfaces ###
 
 ## Go to "Network -> Interfaces" and write the name of the "device" used for the 'WAN' interface.
-WAN="eth1"  # Example: eth0, eth0.2, eth1, eth1.2, wan, etc.
+WAN="wan"  # Example: eth0, eth0.2, eth1, eth1.2, wan, etc.
 
 ######################################################################################################################
 
